5fde350cf2e2202ef6ceac6fb28f9a8a818e464d
shisiyuan14 <1375246695@qq.com>
2023-06-11 14:49:13 +0800

    Turnbased System Updated

--- Assets/Script/TurnbasedSystem.cs
+++ Assets/Script/TurnbasedSystem.cs
@@ -4,29 +4,70 @@ using UnityEngine;
 
 public class TurnbasedSystem : MonoBehaviour
 {
-    public int RoundNumber = 1;
+    public int RoundNumber = 0;
     public int round;
-    public bool IsPlayerDead=true;
+    public bool IsPlayerDead=false;
     public bool IsControlOvered = true;
+
+    public GameObject EndMenu;
 
 
     // Start is called before the first frame update
     void Start()
     {
-       
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        while (!IsPlayerDead)
-        {
-            while(!IsControlOvered)
-            {
+         EndJudge();
 
-            }
-            RoundNumber++;
-            
+    }
+
+    void ControlPhase()
+    {
+
+    }
+
+    void MovePhase()
+    {
+
+    }
+
+    void AttackPhase()
+    {
+
+    }
+
+    void Event1()
+    {
+
+    }
+
+    void Event2()
+    {
+
+    }
+
+    void Event3()
+    {
+
+    }
+
+    void Event4()
+    {
+
+    }
+   
+    void EndJudge()
+    {
+        if(IsPlayerDead)
+        {
+           EndMenu.SetActive(true);
         }
     }
+
+
 }
