6414f0b2202eb6fd5bbb1ebf97ca89fd19cf9a63
shisiyuan14 <46749408+shisiyuan14@users.noreply.github.com>
2023-06-14 10:22:46 +0800

    One turn simulating
    use invoke, need to be changed

--- Assets/Script/TurnbasedSystem.cs
+++ Assets/Script/TurnbasedSystem.cs
@@ -8,37 +8,62 @@ public class TurnbasedSystem : MonoBehaviour
     public int round;
     public bool IsPlayerDead=false;
     public bool IsControlOvered = true;
-
+    public bool IsEvent1Overed = true;
+    public int ControlPhaseTime = 15;
+    public int MovingPhaseTime = 15;
+    public int AttackPhaseTime = 5;
+    public int PlayerHD = 20;
     public GameObject EndMenu;
+    public GameObject ControlMenu;
+    public GameObject MoveMenu;
+    public GameObject EventsMenu;
+    public GameObject AttackMenu;
 
 
     // Start is called before the first frame update
     void Start()
     {
-
+            
+            RoundNumber++;
+            ControlPhase();
+            Invoke("Event2", 15);
+            Invoke("MovePhase", 16);
+            Invoke("Event3", 32);
+            Invoke("AttackPhase", 33);
+            Invoke("Event4", 38);
+        
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(PlayerHD<=0)
+        {
+            IsPlayerDead = true;
+        }
          EndJudge();
 
     }
 
     void ControlPhase()
     {
-
+        ControlMenu.SetActive(true);
+        Debug.Log("ControlPhase");
     }
 
     void MovePhase()
     {
-
+        EventsMenu.SetActive(false);
+        MoveMenu.SetActive(true);
+        Debug.Log("MovePhase");
     }
 
     void AttackPhase()
     {
-
+        EventsMenu.SetActive(false);
+        AttackMenu.SetActive(true);
+        Debug.Log("AttackPhase");
     }
 
     void Event1()
@@ -48,17 +73,25 @@ public class TurnbasedSystem : MonoBehaviour
 
     void Event2()
     {
-
+        ControlMenu.SetActive(false);
+        IsControlOvered=true;
+        EventsMenu.SetActive(true);
+        Debug.Log("Event2");
     }
 
     void Event3()
     {
-
+       MoveMenu.SetActive(false);
+       EventsMenu.SetActive(true);
+        Debug.Log("Event3");
     }
 
     void Event4()
     {
-
+        PlayerHD--;
+        AttackMenu.SetActive(false);
+        EventsMenu.SetActive(true);
+        Debug.Log("Event4");
     }
    
     void EndJudge()
