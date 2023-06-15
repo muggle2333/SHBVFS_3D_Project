8a67bc7ae8e98273a63d3e7b5db3b92bc7770bcb
shisiyuan14 <1375246695@qq.com>
2023-06-15 16:35:20 +0800

    TBS

--- Assets/Script/GameSystem/TurnbasedSystem.cs
+++ Assets/Script/GameSystem/TurnbasedSystem.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class TurnbasedSystem : MonoBehaviour
 {
     public int RoundNumber = 0;
-    public int round;
-    public bool IsPlayerDead=false;
+    public float ControlPhaseTime = 180;
+    public float MovingPhaseTime = 15;
+    public float AttackPhaseTime = 5;
+    public float PlayerHD = 20;
+    public bool IsPlayerDead = false;
     public bool IsControlOvered = true;
     public bool IsEvent1Overed = true;
-    public int ControlPhaseTime = 15;
-    public int MovingPhaseTime = 15;
-    public int AttackPhaseTime = 5;
-    public int PlayerHD = 20;
     public GameObject EndMenu;
     public GameObject ControlMenu;
     public GameObject MoveMenu;
@@ -20,26 +21,20 @@ public class TurnbasedSystem : MonoBehaviour
     public GameObject AttackMenu;
 
 
+    #region Timer
+    public float TimerValue ;
+
+    public TextMeshProUGUI Timer;
+
+    #endregion
+
+    public TextMeshProUGUI Rounds;
+
     // Start is called before the first frame update
     void Start()
     {
-
-
-
+        TimerValue = ControlPhaseTime;
         StartCoroutine("TurnStart");
-
-
-
-
-        //Invoke("Event2", 15);
-        //Invoke("MovePhase", 16);
-        //Invoke("Event3", 32);
-        //Invoke("AttackPhase", 33);
-        //Invoke("Event4", 38);
-
-
-
-
 
     }
 
@@ -52,29 +47,38 @@ public class TurnbasedSystem : MonoBehaviour
         }
          EndJudge();
        
-    }
+       if(TimerValue>0)
+        {
+            TimerValue -= Time.deltaTime;
+        }
+        else
+        {
+            TimerValue = 0;
+        }
+        DisplayTimer(TimerValue);
 
+        Rounds.text = RoundNumber.ToString();
+    }
+    #region One turn
     IEnumerator TurnStart()
     {
         RoundNumber++;
         ControlPhase();
         yield return new WaitForSecondsRealtime(ControlPhaseTime);
-        Event2();
-        MovePhase();
-        yield return new WaitForSecondsRealtime(MovingPhaseTime);
-        Event3();
-        AttackPhase();
-        yield return new WaitForSecondsRealtime(AttackPhaseTime);
-        Event4();
-        
+        StartCoroutine("Event2");
     }
+    #endregion
 
+    #region Single phases
     void ControlPhase()
     {
+        TimerValue = ControlPhaseTime;
         EventsMenu.SetActive(false);
         ControlMenu.SetActive(true);
         Debug.Log("ControlPhase");
+       
     }
+
 
     void MovePhase()
     {
@@ -97,13 +101,18 @@ public class TurnbasedSystem : MonoBehaviour
 
     }
 
-    void Event2()
+    IEnumerator Event2()
     {
         ControlMenu.SetActive(false);
         IsControlOvered=true;
         EventsMenu.SetActive(true);
         Debug.Log("Event2");
-       
+        MovePhase();
+        yield return new WaitForSecondsRealtime(MovingPhaseTime);
+        Event3();
+        AttackPhase();
+        yield return new WaitForSecondsRealtime(AttackPhaseTime);
+        Event4();
     }
 
     void Event3()
@@ -125,15 +134,33 @@ public class TurnbasedSystem : MonoBehaviour
         
        
     }
-   
+    #endregion
+
     void EndJudge()
     {
-        Time.timeScale = 0;
+        //Time.timeScale = 0;
         if(IsPlayerDead)
         {
            EndMenu.SetActive(true);
         }
     }
 
+    #region Display TurnBasedTimer
+   void DisplayTimer(float timeToDisplay)
+    {
+        if(timeToDisplay < 0)
+        {
+            timeToDisplay = 0;
+        }
 
+        Timer.text = Mathf.FloorToInt(TimerValue) .ToString();
+    }
+
+    #endregion
+
+    public void TurnOver()
+    {
+        StopCoroutine("TurnStart");
+        StartCoroutine("Event2");
+    }
 }
