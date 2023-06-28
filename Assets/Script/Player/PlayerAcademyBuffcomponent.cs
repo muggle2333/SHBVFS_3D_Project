aca4d2df9a5ccdafb7196e517bb017fb74a35140
Johnny7753 <115707854+Johnny7753@users.noreply.github.com>
2023-06-28 20:31:46 +0800

    我也不知道弄了些啥

--- Assets/Script/Player/PlayerAcademyBuffcomponent.cs
+++ Assets/Script/Player/PlayerAcademyBuffcomponent.cs
@@ -2,22 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public struct AcademyBuffData
+{
+    public int hp;
+    public int hpPreRound;
+    public int attackRange;
+    public int attackDamage;
+    public int defense;
+    public int APPerRound;
+}
 public class PlayerAcademyBuffcomponent : MonoBehaviour
 {
-
-    public Dictionary<AcademyType, int> AcademyBuff = new Dictionary<AcademyType, int>();
+    //public Dictionary<AcademyType, Dictionary<int,AcademyBuffData>> AcademyBuff = new Dictionary<AcademyType, Dictionary<int, AcademyBuffData>>();
     // Start is called before the first frame update
+    public Player player;
+
+    public List<int>[][] academyBuff;
     void Start()
+    {
+        //AcademyBuff.Add(AcademyType.YI, ( 0,  ));
+    }
+    
+    // Update is called once per frame
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UpdatePlayerAcademyBuff()
     {
-        if (this.GetComponent<Player>().academyOwnedPoint[0] == 1)
-        {
-
-        }
+        
     }
 }
