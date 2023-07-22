ad0ddbc2455d2aa3b046a1415c03e5c19186d7ac
fucangyun <76155504+fucangyun@users.noreply.github.com>
2023-07-22 21:19:50 +0800

    update camera

--- Assets/Script/Camera/CameraTest4.cs
+++ Assets/Script/Camera/CameraTest4.cs
@@ -1,25 +1,44 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
+using System;
 
 public class CameraTest4 : MonoBehaviour
 {
     [SerializeField] private Vector3 moveSpeed = new Vector3(10f, 10f, 10f);
     [SerializeField] private Vector3 rotateSpeed = new Vector3(10f, 10f, 10f);
     [SerializeField] private Transform cam;
-    float x = 0f;
-
+    private Vector3 pos = new Vector3(3, 13, -18);
+    private float x = 0f;
+    private bool isMoving = false;
+    private Vector3 vec = new Vector3();
+    private Guid uid;
     private void Start()
     {
         var angles = transform.eulerAngles;
         x = angles.y;
+        vec = transform.position;
     }
     void Update()
     {
+        //if(isMoving && 
+        //    (Input.GetMouseButtonDown(2) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D)))
+        //{
+        //    DOTween.Kill(uid);
+        //    vec = transform.position;   
+        //    isMoving = false;
+        //    Debug.LogWarning(111111);
+        //}
         MoveCamera();
         ScaleCamera();
-        RoateCamera();
+        RotateCamera();
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            FocusGrid();
+        }
     }
 
     private void MoveCamera()
@@ -41,13 +60,35 @@ public class CameraTest4 : MonoBehaviour
         }
     }
 
-    private void RoateCamera()
+    private void RotateCamera()
     {
-        if(Input.GetMouseButton(2))
+
+        if (Input.GetMouseButton(2))
         {
             x += Input.GetAxis("Mouse X") * rotateSpeed.x * 0.05f;
             var rotation = Quaternion.Euler(0, x, 0);
             transform.rotation = rotation;
         }
+    }
+
+    public void FocusGrid()
+    {
+        var seq = DOTween.Sequence();
+        //isMoving = true;
+        var selectedGridObject = SelectManager.Instance.GetLatestGridObject();
+        if (selectedGridObject != null)
+        {
+            vec = GridManager.Instance.grid.GetWorldPositionCenter(selectedGridObject.x, selectedGridObject.z);
+        }
+        else
+        {
+            vec = GameplayManager.Instance.currentPlayer.transform.position;
+        }
+        //var uid = System.Guid.NewGuid();
+        //seq.id = uid;
+        seq.Append(transform.DOMove(vec, 0.5f));
+        seq.Join(cam.DOLocalMove(pos, 0.5f));
+        //cam.DOMoveZ(-19, 1f);
+        //seq.AppendCallback(() => { isMoving = false; });
     }
 }
