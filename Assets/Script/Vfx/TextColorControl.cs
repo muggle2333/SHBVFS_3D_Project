4b17816dc20913ca99a3393290172dd75c960d7a
fucangyun <76155504+fucangyun@users.noreply.github.com>
2023-07-29 13:19:06 +0800

    fix text blink and camera boundary bug

--- Assets/Script/Vfx/TextColorControl.cs
+++ Assets/Script/Vfx/TextColorControl.cs
@@ -11,14 +11,13 @@ public class TextColorControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //ColorBlink(new Color32(231, 42, 0, 255), 0.4f);
+        //ColorBlink(new Color32(234, 201, 121, 255), new Color32(231, 42, 0, 255), 0.4f);
     }
 
-    public void ColorBlink(Color32 targetcolor, float duration)
+    public void ColorBlink(Color32 formerColor, Color32 targetColor, float duration)
     {
-        Color32 formercolor = transform.GetComponent<Text>().color;
         var seq = DOTween.Sequence();
-        seq.Append(DOTween.To(() => transform.GetComponent<Text>().color, x => transform.GetComponent<Text>().color = x, targetcolor, duration / 2));
-        seq.Append(DOTween.To(() => transform.GetComponent<Text>().color, x => transform.GetComponent<Text>().color = x, formercolor, duration / 2));
+        seq.Append(DOTween.To(() => transform.GetComponent<Text>().color, x => transform.GetComponent<Text>().color = x, targetColor, duration / 2));
+        seq.Append(DOTween.To(() => transform.GetComponent<Text>().color, x => transform.GetComponent<Text>().color = x, formerColor, duration / 2));
     }
 }
