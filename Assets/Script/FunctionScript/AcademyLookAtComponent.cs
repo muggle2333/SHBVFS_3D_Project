6a142b1b29b241582e43c7ab04375ae0cdecdd2b
fucangyun <76155504+fucangyun@users.noreply.github.com>
2023-07-29 17:47:08 +0800

    fix building bug

--- Assets/Script/FunctionScript/AcademyLookAtComponent.cs
+++ Assets/Script/FunctionScript/AcademyLookAtComponent.cs
@@ -1,19 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
-using TreeEditor;
 using UnityEngine;
 
 public class AcademyLookAtComponent : MonoBehaviour
 {
-    //private Transform cameraRotation;
-    void Start()
-    {
-        //cameraRotation = FindObjectOfType<CameraTest4>().gameObject.transform;
-    }
-
-    void Update()
-    {
-        //transform.rotation = cameraRotation.rotation;
-    }
+    
 
 }
